8dfc1cc02c1a940a4f94c08084ea1b52a08ab6c3
william535230174 <william.535230174@stu.untar.ac.id>
2025-10-30 23:35:17 +0700

    bub bub

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const images = Array.from({ length: 96 }).map(
+const images = Array.from({ length: 115 }).map(
     (_, i) => `/album/${i + 1}.webp`
 );
 
